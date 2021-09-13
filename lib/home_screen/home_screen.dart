4a5c8e81e07ad8b48b4f8f323be9e2ed0d9bbd54
jerygit <jery.batman@gmail.com>
2021-09-13 17:16:32 +0700

    commit timenow

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:covidapp/model/covid_model.dart';
 import 'package:covidapp/resource/getapi.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
 
@@ -11,66 +15,94 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
     late Future<CovidModel> futureCovid;
     //CovidModel nv=CovidModel("KA", data:{"S","A","SA","Ấ"} );
+       String? _timeString;
 
   @override
-  void initState() {
-    // TODO: implement initState
-    
+  void initState(){
+    // _timeString = "${DateTime.now().hour} : ${DateTime.now().minute} :${DateTime.now().second}";
+    // Timer.periodic(Duration(seconds:1), (Timer t)=>_getCurrentTime());
     super.initState();
-   // futureCovid = fetchDataCovid();
   }
+
+  
   @override
   Widget build(BuildContext context) {
   //  int current=0;
-  
+    _timeString = "${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}";
+
     return Scaffold(
       
    body: TabBarView(children: 
        [
     
                    new Container(
-                            child: FutureBuilder<List<CovidModel>>(
-                            future: getApi().fetchCovidList(),
-                           builder: (BuildContext context, AsyncSnapshot? snapshot){
-                            
-                             if(snapshot!.hasData){
-                                List<CovidModel> videoList = snapshot.data!;
-                               return ListView.builder(
-                     itemCount: snapshot.data.length,
-                     itemBuilder: (BuildContext context, int index){
-                      // int ina=index-snapshot.data.length
-                      //.data[index]['picture']['large'])),
-                       return  ListTile(                 
-                          title: Text(videoList[index].tinh??"",
-                         style:TextStyle(color: Colors.black,fontSize: 20)),
-                         subtitle: Text("Tổng Ca nhiễm nay: ${snapshot.data[index]?.nhiem??""}") , 
-                        // leading: CircleAvatar(
-                        //         radius: 30,
-                        //         backgroundImage: NetworkImage(snapshot.data[index]['picture']['large'])),
-                        
-                       );
-                    
-                     }
-
-
+                            child: Column(
+                              children: [
+                               Padding(
+                                 padding: const EdgeInsets.all(8.0),
                                
-                               );
-                             }
-                             else if(snapshot.hasError){
-                               return Container(
-                     child: Center(child: Text('Ko tim thay data'),)
-                               );
-                             }
-                             else 
-                             return Container(
-                               child: Center(child: CircularProgressIndicator(),),
-                             );
-                           //return snapshot.data;
-                           },
-                          
-                          ),
-                          
-                        )
+                                 child: new Container(child: 
+                                 
+                                 
+                                 
+                                 
+                                 
+                                 new Text("Nguồn: Bộ Y Tế. $_timeString ", style: TextStyle(color:Color(0xFF3F5AA6)),)),
+                               ),
+                                //"Nguồn: Bộ Y Tế, Thời gian: "
+                                
+                                     //s         DateFormat('MM/dd/yyyy hh:mm:ss').format(DateTime.now()),
+
+                                ///TEST TIME///////////////////////////////
+                                ///
+                                  Expanded(
+                                   flex: 3, 
+                                    child: FutureBuilder<List<CovidModel>>(
+                                                                future: getApi().fetchCovidList(),
+                                                             builder: (BuildContext context, AsyncSnapshot? snapshot){
+                                                                
+                                                                 if(snapshot!.hasData){
+                                    List<CovidModel> videoList = snapshot.data!;
+                                                                   return ListView.builder(
+                                                       itemCount: snapshot.data.length,
+                                                       itemBuilder: (BuildContext context, int index){
+                                                        // int ina=index-snapshot.data.length
+                                                        //.data[index]['picture']['large'])),
+                                                         return  ListTile(                 
+                                                            title: Text(videoList[index].tinh??"",
+                                                           style:TextStyle(color: Colors.black,fontSize: 20)),
+                                                           subtitle: Text("Ca nhiễm hôm nay: ${snapshot.data[index]?.nhiem??""}") , 
+                                                          // leading: CircleAvatar(
+                                                          //         radius: 30,
+                                                          //         backgroundImage: NetworkImage(snapshot.data[index]['picture']['large'])),
+                                                          
+                                                         );
+                                                      
+                                                       }
+                                  
+                                  
+                                                                   
+                                                                   );
+                                                                 }
+                                                                 else if(snapshot.hasError){
+                                                                   return Container(
+                                                       child: Center(child: Text('Ko tim thay data'),)
+                                                                   );
+                                                                 }
+                                                                 else 
+                                                                 return Container(
+                                                                   child: Center(child: CircularProgressIndicator(),),
+                                                                 );
+                                                             //return snapshot.data;
+                                                             },
+                                                            
+                                                            ),
+                                  ),
+                            
+                              ],
+                            )
+   )                          
+                        
    
       
       ,Container(child: Text("data2"),)
@@ -82,7 +114,17 @@ class _HomeScreenState extends State<HomeScreen> {
    )
     );
   }
+
+  _getCurrentTime() {
+    setState(() {
+  _timeString = "${DateTime.now().day} / ${DateTime.now().month} /${DateTime.now().year}";
+    });
+  }
+
+  
 }
+
+
 
 
 
